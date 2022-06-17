33e604769e961c7a2d484183603f9bd8bdeeeb60
Adam <adaand00@gmail.com>
2022-06-17 20:17:03 +0200

    Mouse code, PCB finished

--- 2-GRIDNAVIGATOR/Code/code.py
+++ 2-GRIDNAVIGATOR/Code/code.py
@@ -1,5 +1,26 @@
 import rotaryio
 import board
+import usb_hid
+import digitalio
+import time
+from adafruit_hid.mouse import Mouse
+
+mouse = Mouse(usb_hid.devices)
+
+# set pin 5 as input pullup (right click)
+right_click = digitalio.DigitalInOut(board.D5)
+right_click.direction = digitalio.Direction.INPUT
+right_click.pull = digitalio.Pull.UP
+
+# set pin 4 as input pullup (left click)
+left_click = digitalio.DigitalInOut(board.D4)
+left_click.direction = digitalio.Direction.INPUT
+left_click.pull = digitalio.Pull.UP
+
+# mode switch on pin D7
+mode_sw = digitalio.DigitalInOut(board.D7)
+mode_sw.direction = digitalio.Direction.INPUT
+
 
 keymap = [
     ['a', 'b', 'c', 'd', 'e'],
@@ -14,21 +35,53 @@ keymap = [
 encoderX = rotaryio.IncrementalEncoder(board.D0, board.D1)
 encoderY = rotaryio.IncrementalEncoder(board.D2, board.D3)
 
+lastEncoderX = 0
+lastEncoderY = 0
+
+x = 0
+y = 0
+
 last_letter = 'a'
 
+mode = mode_sw.value
+
 while True:
-    # Get the current position of the encoder
-    x = encoderX.position
-    y = encoderY.position
+    if mode == 0:
+        if right_click.value == 0:
+            mouse.click(mouse.RIGHT_BUTTON)
+            time.sleep(0.2)
 
-    # constrain x and y positions
-    x = min(max(x, 0), len(keymap) - 1)
-    y = min(max(y, 0), len(keymap[0]) - 1)
+        if left_click.value == 0:
+            mouse.click(mouse.LEFT_BUTTON)
+            time.sleep(0.2)
 
-    # get the letter at the current position
-    letter = keymap[x][y]
+        if encoderX.position != lastEncoderX:
+            mouse.move(x=(encoderX.position - lastEncoderX)*10)
+            lastEncoderX = encoderX.position
 
-    # if the letter has changed, print it
-    if letter != last_letter:
-        print(letter)
-        last_letter = letter
+        if encoderY.position != lastEncoderY:
+            mouse.move(y=(encoderY.position - lastEncoderY)*10)
+            lastEncoderY = encoderY.position
+
+    else:
+        # Get the current position of the encoder
+        x += encoderX.position - lastEncoderX
+        y += encoderY.position - lastEncoderY
+
+        # Update the last position
+        lastEncoderX = encoderX.position
+        lastEncoderY = encoderY.position
+
+        # constrain x and y positions
+        x = min(max(x, 0), len(keymap) - 1)
+        y = min(max(y, 0), len(keymap[0]) - 1)
+
+        # get the letter at the current position
+        letter = keymap[x][y]
+
+        # if the letter has changed, print it
+        if letter != last_letter:
+            print(letter)
+            last_letter = letter
+
+    mode = mode_sw.value
